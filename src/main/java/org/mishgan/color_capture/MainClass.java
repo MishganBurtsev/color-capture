459614443c2464c617af33d654ace7ee00b94942
Михаил <MikeDefenit@yandex.ru>
2025-12-26 06:03:56 +0300

    Init commit

---
+++ src/main/java/org/mishgan/color_capture/MainClass.java
@@ -0,0 +1,12 @@
+package org.mishgan.color_capture;
+
+public class MainClass {
+
+    public static String myName() {
+        return "MainClass";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/org/mishgan/color_capture/MainClassTest.java
@@ -0,0 +1,13 @@
+package org.mishgan.color_capture;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainClassTest {
+
+    @Test
+    public void testMyName() {
+        assertEquals("MainClass", MainClass.myName());
+    }
+}
