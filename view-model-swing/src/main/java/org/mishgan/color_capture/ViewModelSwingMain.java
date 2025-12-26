c81138fcbda212ce6fabf41daef18e78c4df132a
Михаил <MikeDefenit@yandex.ru>
2025-12-26 11:43:46 +0300

    Add logic and view model

---
+++ view-model-swing/src/main/java/org/mishgan/color_capture/ViewModelSwingMain.java
@@ -0,0 +1,7 @@
+package org.mishgan.color_capture;
+
+public class ViewModelSwingMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ view-model-swing/src/main/java/org/mishgan/color_capture/frame/MainMenuFrame.java
@@ -0,0 +1,6 @@
+package org.mishgan.color_capture.frame;
+
+import javax.swing.*;
+
+public class MainMenuFrame extends JFrame {
+}
